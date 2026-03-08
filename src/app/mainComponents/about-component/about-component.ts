d9b8624d20bcf93cf58375be6565a90c18867370
Dilan Lakshithae <lakshithadilan6@gmail.com>
2026-03-08 10:04:02 +0530

    Home changes

--- src/app/mainComponents/about-component/about-component.ts
+++ src/app/mainComponents/about-component/about-component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContactUsComponent } from '../../sharedComponents/contact-us-component/contact-us-component';
 
 @Component({
   selector: 'app-about-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ContactUsComponent],
   templateUrl: './about-component.html',
   styleUrl: './about-component.css'
 })
 export class AboutComponent {
-
+  homecontact = true;
 }
