3df8e3137ae57f71844f2fcf817b44fa6b35ab53
Dilan Lakshitha <lakshithadilan6@gmail.com>
2026-07-14 21:53:23 +0530

    domain change

--- api/send-booking-email.js
+++ api/send-booking-email.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.ceylonjojotravels.com");
+  res.setHeader("Access-Control-Allow-Origin", "https://ceylonjojotravels.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
